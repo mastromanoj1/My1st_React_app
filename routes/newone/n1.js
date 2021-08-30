3653bb5c3e239e0d116ffd63306e4c2877705da0
mightymk <mastromanoj1@gmail.com>
2021-08-30 19:24:54 +0530

    n1

---
+++ routes/newone/n1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class New extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { mk:"Smample" }
+    }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default New;
